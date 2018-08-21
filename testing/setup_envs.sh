4d49eb567cedfb022c22e2c95b2abf49cc70388d
Jim Crist <jiminy.crist@gmail.com>
2018-08-21 01:04:13 -0500

    fixup2

--- testing/setup_envs.sh
+++ testing/setup_envs.sh
@@ -1,5 +1,5 @@
 #/usr/bin/env bash
-set -xe
+set -e
 
 echo "== Setting up environments for testing =="
 
@@ -8,14 +8,10 @@ current_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 echo "Creating simple environment"
 envpath="${current_dir}/environments/simple"
 python -m venv $envpath
-source "${envpath}/bin/activate"
-pip install toolz
-deactivate
+${envpath}/bin/pip install toolz
 
 echo "Creating editable environment"
 envpath="${current_dir}/environments/editable"
 python -m venv $envpath
-source "${envpath}/bin/activate"
-pip install toolz
-pushd "${current_dir}/.." && python setup.py develop && popd
-deactivate
+${envpath}/bin/pip install toolz
+pushd "${current_dir}/.." && ${envpath}/bin/python setup.py develop && popd
