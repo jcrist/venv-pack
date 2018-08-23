85dcc4401e557610ef2a108e6c8b3cb79adf5516
Travis CI User <travis@example.org>
2018-08-23 01:21:16 +0000

    Update docs after building Travis build 26 of jcrist/venv-pack
    The docs were built from the branch 'master' against the commit
    9db38e5a8546257c1532944017af1b5346f91629.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/jcrist/venv-pack/jobs/419445866.
    
    The doctr command that was run is
    
        /home/travis/miniconda/envs/testenv/bin/doctr deploy . --built-docs docs/build/html/

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.0+2.gcada93b',
+    VERSION: '0.1.0+5.g9db38e5',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
